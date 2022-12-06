0dd860601ce52adef7b2a46f0c9c32cc88187910
Jhon Rey <jhonreymendiola@gmail.com>
2022-12-07 01:00:36 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'velocity-tracking';
+
+  employees:any =[
+    {employee: "",estimated_hours: "",actual_hours: ""},
+  ]
+
+  // employeeArr:any =[]
+
+  // employeeObj:any = {
+  //   employee: "",
+  //   estimated_hours: "",
+  //   actual_hours: ""}
+
+  content:any = {
+    project: "",
+    task_title: "",
+    task_description: "",
+    total_estimate: "",
+    employees: this.employees
+  }
+  add(){
+    this.employees.push({employee: ""})
+  }
+  del(i:number){
+    this.employees.splice(i, 1)
+  }
+  save(){
+    console.log(this.content)
+    // this.employeeArr.push(this.employeeObj)
+    // console.log(this.employees)
+  }
+  close(){
+    location.reload();
+  }
 }
+
